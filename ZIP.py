a94854f6c8870561377e84524f57fd6249124ce8
ColdVoid <52884856+Colcactus-ColdVoid@users.noreply.github.com>
2020-12-12 23:51:38 +0800

    Update ZIP.py

--- ZIP.py
+++ ZIP.py
@@ -12,7 +12,21 @@ def main():
             index[hex0]=count
             count=count+1
         key[index[hex0]]+=1
-        
+
+        if(index[hex0]!=0 and key[index[hex0]]>key[index[hex0]-1]):
+            a=key[index[hex0]-1]
+            key[index[hex0]-1]=key[index[hex0]]
+            key[index[hex0]]=a
+
+            a=value[index[hex0]-1]
+            b=value[index[hex0]]
+            value[index[hex0]-1]=value[index[hex0]]
+            value[index[hex0]]=a
+
+            c=index[a]
+            index[a]=index[b]
+            index[b]=c
+            
     print(index)
     print(value)
     print(key)
